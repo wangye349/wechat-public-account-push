bbd2fe3961f8e0fd00828301b86782a443cb862e
wangye349 <wangy349@mail2.sysu.edu.cn>
2024-08-25 23:03:23 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -21,7 +21,7 @@ const USER_CONFIG =
         {
             "name": "yu",
             "id": "oilFW6VDSF4gtpiR_gvFY0vGQYzM",
-            "useTemplateId": "TQLcO-79XQwVnw-ThE-izBFg6sG70pIeb5bvBFlWqNo",
+            "useTemplateId": "b7ZZ9MpaLsnXRNn2-NbQI2UMTTi-IY77WACus3ExtYY",
             "province": "广东省",
             "city": "深圳市",
             "horoscopeDate": "08-25",
