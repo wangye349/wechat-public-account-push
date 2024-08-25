46f48bdc3c7c9d4f854773f5b08a37779c22028b
wangye349 <wangy349@mail2.sysu.edu.cn>
2024-08-25 22:47:12 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -20,7 +20,7 @@ const USER_CONFIG =
     "USERS": [
         {
             "name": "yu",
-            "id": "oilFW6VDSF4gtpiR_gvFY0vGQYzM",
+            "id": "oilFW6cAm5DCLL--_yydp4DdUnO8",
             "useTemplateId": "_Uj-sQpzpajfKOeHncJIbbSPZkLByTM78EE6FXwg7H4",
             "province": "广东省",
             "city": "深圳市",
